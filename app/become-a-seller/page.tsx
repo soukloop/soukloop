cefa432ff7013d2feb17dfc794d9c735d4fb6e93
M Tahir Shabbir <tahirshabbir955@gmail.com>
2026-02-20 14:48:37 +0500

    changes in frontend

--- app/become-a-seller/page.tsx
+++ app/become-a-seller/page.tsx
@@ -6,7 +6,7 @@ import FooterSection from '@/components/footer-section'
 
 export default function BecomeSellerPage() {
     return (
-        <main className="min-h-screen bg-white">
+        <main className="min-h-screen bg-white sm:mt-[-13.5rem] mt-[-11.2rem]">
             <EcommerceHeader />
             <div className="pt-20"> {/* Offset for fixed header */}
                 <BecomeSellerCTA />
